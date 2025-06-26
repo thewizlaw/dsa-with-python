6673de424ed6306f68bb23bb028e4ed090fa90cf
thewizlaw <pikwik458@gmail.com>
2025-06-26 19:05:29 +0530

    leetcode

---
+++ most_occur.py
@@ -0,0 +1,11 @@
+from collections import Counter
+def main():
+    nums = [3,2,3]
+    mosts = most(nums)
+    print(mosts)
+
+def most(nums):
+    counter = Counter(nums)
+    return counter.most_common(1)[0][0]
+
+main()
